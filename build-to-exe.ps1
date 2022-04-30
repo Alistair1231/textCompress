d8f65fcf83f32718abe8c95b59009c69d130d34b
Alistair Bahr <alistairbahr@outlook.com>
2022-04-30 22:14:21 +0200

    moved all to /src

--- build-to-exe.ps1
+++
@@ -1,2 +0,0 @@
-# courtesy of auto-py-to-exe
-pyinstaller --noconfirm --onefile --windowed  "C:/Users/alima/MEGAsync/_ProgStuff/textCompress/main.py"
---
+++ src/build-to-exe.ps1
@@ -0,0 +1,2 @@
+# courtesy of auto-py-to-exe
+pyinstaller --noconfirm --onefile --windowed  "C:/Users/alima/MEGAsync/_ProgStuff/textCompress/main.py"
